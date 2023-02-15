49371634ad07654698a47ecb381caf3246ed22f3
Ryan O'Neill <ryanoneill416@gmail.com>
2023-02-15 19:10:37 +0000

    Implement React Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Wagwarn World</h1>
+      <Button variant='primary'>Wagwarn World but as a button</Button>
     </div>
   );
 }
