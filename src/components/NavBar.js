1d4420c949856091a25566fa7c494d7885d72cd5
Ryan O'Neill <ryanoneill416@gmail.com>
2023-02-15 20:50:21 +0000

    Create NavBar.module.css to add styling to the navbar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,15 +3,16 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import logo from '../assets/tatted-logo-no-bg.png';
+import styles from '../styles/NavBar.module.css';
 
 const NavBar = () => {
   return (
-    <Navbar bg="dark" variant="dark" expand="md" fixed="top">
+    <Navbar variant="dark" expand="md" fixed="top" className={styles.NavBar}>
       <Container>
         <Navbar.Brand><img src={logo} alt="logo" height="55"/></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
+          <Nav className="ml-auto text-center">
             <Nav.Link><i className="fa-solid fa-house"/>Home</Nav.Link>
             <Nav.Link><i className="fa-solid fa-right-to-bracket"/>Sign In</Nav.Link>
             <Nav.Link><i className="fa-solid fa-user-plus"/>Sign Up</Nav.Link>
