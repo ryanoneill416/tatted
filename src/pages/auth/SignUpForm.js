5bfb59fa26c891db96e05ec12bf49b79e99847e3
Ryan O'Neill <ryanoneill416@gmail.com>
2023-02-16 19:19:05 +0000

    Add provisional sign up form form and add styling to signinupform css

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -25,7 +25,49 @@ const SignUpForm = () => {
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>sign up</h1>
 
-          {/* add your form here */}
+          <Form>
+            <Form.Group controlId="username">
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control
+                className={styles.Input}
+                type="text"
+                placeholder="Username"
+                name="username"
+              />
+            </Form.Group>
+
+            <Form.Group controlId="password1">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+                className={styles.Input}
+                type="password"
+                placeholder="Password"
+                name="password1"
+              />
+            </Form.Group>
+
+            <Form.Group controlId="password2">
+              <Form.Label className="d-none">Confirm Password</Form.Label>
+              <Form.Control
+                className={styles.Input}
+                type="password"
+                placeholder="Confirm Password"
+                name="password2"
+              />
+            </Form.Group>
+
+            <Form.Group controlId="is_artist">
+              <Form.Check
+                className={styles.Check}
+                type="checkbox"
+                label="Join as a tattoo artist"
+              />
+            </Form.Group>
+
+            <Button className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Black}`} variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
           <Link className={styles.Link} to="/signin">
