28ceafa45c7436e1e1e649c2b576939f94b5ec60
Ryan O'Neill <ryanoneill416@gmail.com>
2023-02-18 17:09:40 +0000

    Add setErrors useState hook to retrieve errors made while trying to sign in

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -21,6 +21,8 @@ function SignInForm() {
   });
   const { username, password } = signInData;
 
+  const { errors, setErrors } = useState({});
+
   const history = useHistory();
 
   const handleChange = (event) => {
@@ -38,7 +40,7 @@ function SignInForm() {
       await axios.post("/dj-rest-auth/login/", signInData);
       history.push("/");
     } catch (err) {
-      console.log(err);
+      setErrors(err.response?.data);
     }
   };
 
@@ -59,7 +61,7 @@ function SignInForm() {
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={`${styles.Header} pb-3`}>sign in</h1>
 
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group controlId="username">
               <Form.Label className="d-none">Username</Form.Label>
               <Form.Control
@@ -67,10 +69,15 @@ function SignInForm() {
                 type="text"
                 placeholder="Username"
                 name="username"
-                // value={username}
-                // onChange={handleChange}
+                value={username}
+                onChange={handleChange}
               />
             </Form.Group>
+            {errors.username?.map((message, idx) => (
+              <Alert key={idx} variant="warning">
+                {message}
+              </Alert>
+            ))}
 
             <Form.Group controlId="password">
               <Form.Label className="d-none">Password</Form.Label>
@@ -79,10 +86,15 @@ function SignInForm() {
                 type="password"
                 placeholder="Password"
                 name="password"
-                // value={password}
-                // onChange={handleChange}
+                value={password}
+                onChange={handleChange}
               />
             </Form.Group>
+            {errors.password?.map((message, idx) => (
+              <Alert key={idx} variant="warning">
+                {message}
+              </Alert>
+            ))}
 
             <Button
               className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Black}`}
@@ -91,6 +103,11 @@ function SignInForm() {
             >
               Sign In
             </Button>
+            {errors.non_field_errors?.map((message, idx) => (
+              <Alert key={idx} variant="warning" className="mt-3">
+                {message}
+              </Alert>
+            ))}
           </Form>
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
