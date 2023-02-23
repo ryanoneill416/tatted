5b2212fbd2aa12665e5ba80dfb73d81264f2e2ff
Ryan O'Neill <ryanoneill416@gmail.com>
2023-02-23 17:29:38 +0000

    Create CommentCreateForm component as well as accompanying CommentCreateEditForm stylesheet

--- src/pages/posts/PostDetailPage.js
+++ src/pages/posts/PostDetailPage.js
@@ -9,35 +9,39 @@ import { useParams } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
 
+import CommentCreateForm from "../comments/CommentCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+
 function PostDetailPage() {
   const { id } = useParams();
-  const [post, setPost] = useState({ results: [] })
+  const [post, setPost] = useState({ results: [] });
+
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] });
 
   useEffect(() => {
     const handleMount = async () => {
-        try {
-            const [{ data: post }] = await Promise.all([
-                axiosReq.get(`/posts/${id}`),
-            ])
-            setPost({results: [post]})
-            console.log(post)
-        } catch(err) {
-            console.log(err)
-        }
-    }
+      try {
+        const [{ data: post }] = await Promise.all([
+          axiosReq.get(`/posts/${id}`),
+        ]);
+        setPost({ results: [post] });
+        console.log(post);
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-    handleMount()
-  }, [id])
-
+    handleMount();
+  }, [id]);
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popping artists mobile</p>
-        <Post {...post.results[0]} setPosts={setPost} postDetailPage/>
-        <Container className={appStyles.Content}>
-          Comments
-        </Container>
+        <Post {...post.results[0]} setPosts={setPost} postDetailPage />
+        <Container className={appStyles.Content}>Comments</Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
         Popping artists desktop
