901a6f421d7565a13a9fe29da76de6af2a3c199e
Ryan O'Neill <ryanoneill416@gmail.com>
2023-02-23 20:50:38 +0000

    Create PoppingArtists component

---
+++ src/pages/profiles/PoppingArtists.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import styles from "../../App.module.css";
+
+const PoppingArtists = () => {
+  return <Container className={styles.Content}>
+    <p>Most Poppin' Artists</p>
+  </Container>;
+};
+
+export default PoppingArtists;
