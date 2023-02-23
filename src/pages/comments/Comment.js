60fb879cbf810ae87f61968ba9d59120fc77c212
Ryan O'Neill <ryanoneill416@gmail.com>
2023-02-23 19:11:38 +0000

    successfully implement handleDelete function to allow comment owners to delete their comments

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,15 +1,44 @@
 import React from "react";
 import Media from "react-bootstrap/Media";
 import { Link } from "react-router-dom";
+import { axiosRes } from "../../api/axiosDefaults";
 import Avatar from "../../components/Avatar";
 import { DropdownOptions } from "../../components/DropdownOptions";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import styles from "../../styles/Comment.module.css";
 
 const Comment = (props) => {
-  const { profile_id, profile_image, owner, updated_at, content } = props;
-  const currentUser = useCurrentUser()
+  const {
+    profile_id,
+    profile_image,
+    owner,
+    updated_at,
+    content,
+    id,
+    setPost,
+    setComments,
+  } = props;
+  const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
+
+  const handleDelete = async () => {
+    try {
+      await axiosRes.delete(`/comments/${id}/`);
+      setPost((prevPost) => ({
+        results: [
+          {
+            ...prevPost.results[0],
+            comments_count: prevPost.results[0].comments_count - 1,
+          },
+        ],
+      }));
+
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: prevComments.results.filter((comment) => comment.id !== id),
+      }));
+    } catch (err) {}
+  };
 
   return (
     <div>
@@ -24,7 +53,7 @@ const Comment = (props) => {
           <p>{content}</p>
         </Media.Body>
         {is_owner && (
-          <DropdownOptions handleEdit={()=>{}} handleDelete={()=>{}} />
+          <DropdownOptions handleEdit={() => {}} handleDelete={handleDelete} />
         )}
       </Media>
     </div>
--- src/pages/posts/PostDetailPage.js
+++ src/pages/posts/PostDetailPage.js
@@ -24,13 +24,12 @@ function PostDetailPage() {
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{ data: post }, {data: comments}] = await Promise.all([
+        const [{ data: post }, { data: comments }] = await Promise.all([
           axiosReq.get(`/posts/${id}`),
-          axiosReq.get(`/comments/?post=${id}`)
+          axiosReq.get(`/comments/?post=${id}`),
         ]);
         setPost({ results: [post] });
-        setComments(comments)
-        console.log(post);
+        setComments(comments);
       } catch (err) {
         console.log(err);
       }
@@ -46,7 +45,7 @@ function PostDetailPage() {
         <Post {...post.results[0]} setPosts={setPost} postDetailPage />
         <Container className={appStyles.Content}>
           {currentUser ? (
-            <CommentCreateForm 
+            <CommentCreateForm
               profile_id={currentUser.profile_id}
               profile_image={profile_image}
               post={id}
@@ -56,15 +55,18 @@ function PostDetailPage() {
           ) : comments.results.length ? (
             "Comments:"
           ) : null}
-          {comments.results.length ? (
-            comments.results.map((comment) => (
-              <Comment key={comment.id} {...comment}/>
-            ))
-          ) : currentUser ? (
-            null
-          ) : (
-            "No comments to be found... yet ;)"
-          )}
+          {comments.results.length
+            ? comments.results.map((comment) => (
+                <Comment
+                  key={comment.id}
+                  {...comment}
+                  setPost={setPost}
+                  setComments={setComments}
+                />
+              ))
+            : currentUser
+            ? null
+            : "No comments to be found... yet ;)"}
         </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
