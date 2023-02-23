3668524b1da4339496fcf3aeedb7f879736f1dba
Ryan O'Neill <ryanoneill416@gmail.com>
2023-02-23 18:13:33 +0000

    Implement API call for fetching post comments within the PostDetailComponent

--- src/pages/posts/PostDetailPage.js
+++ src/pages/posts/PostDetailPage.js
@@ -23,10 +23,12 @@ function PostDetailPage() {
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{ data: post }] = await Promise.all([
+        const [{ data: post }, {data: comments}] = await Promise.all([
           axiosReq.get(`/posts/${id}`),
+          axiosReq.get(`/comments/?post=${id}`)
         ]);
         setPost({ results: [post] });
+        setComments(comments)
         console.log(post);
       } catch (err) {
         console.log(err);
