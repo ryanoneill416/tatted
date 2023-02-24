8942e4bbde9e7e37c9b5ef9c54284b89618f32e0
Ryan O'Neill <ryanoneill416@gmail.com>
2023-02-24 16:30:53 +0000

    Implement rendering and styling of profile components on desktop with buttons allowing authenticated users to follow other artists

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -4,6 +4,7 @@ import btnStyles from "../../styles/Button.module.css";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
+import { Button } from "react-bootstrap";
 
 const Profile = (props) => {
   const { profile, mobile, imageSize = 55 } = props;
@@ -12,16 +13,38 @@ const Profile = (props) => {
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
 
-  return <div className={`d-flex align-items-center ${mobile && "flex-column"}`}>
-    <div>
+  return (
+    <div className={`d-flex align-items-center ${mobile && "flex-column"}`}>
+      <div>
         <Link className="align-self-center" to={`/profiles/${id}`}>
-            <Avatar src={image} height={imageSize} />
+          <Avatar src={image} height={imageSize} />
         </Link>
-    </div>
-    <div className={`mx-2 ${styles.WordBreak}`}>
+      </div>
+      <div className={`mx-2 ${styles.WordBreak}`}>
         <p>{owner}</p>
+      </div>
+      <div className={`text-right ${!mobile && "ml-auto"}`}>
+        {!mobile &&
+          currentUser &&
+          !is_owner &&
+          (following_id ? (
+            <button
+              className={`${btnStyles.Button} ${btnStyles.Secondary}`}
+              onClick={() => {}}
+            >
+              Unfollow
+            </button>
+          ) : (
+            <button
+              className={`${btnStyles.Button} ${btnStyles.Black}`}
+              onClick={() => {}}
+            >
+              Follow
+            </button>
+          ))}
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default Profile;
