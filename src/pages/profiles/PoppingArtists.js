e01c31e82e9b1179269aaac89f172765600f8c97
Ryan O'Neill <ryanoneill416@gmail.com>
2023-02-24 16:22:14 +0000

    Implement styling rendering of profile component within poppingArtists component for mobile

--- src/pages/profiles/PoppingArtists.js
+++ src/pages/profiles/PoppingArtists.js
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import { axiosReq } from "../../api/axiosDefaults";
 import styles from "../../App.module.css";
+import Asset from "../../components/Asset";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import Profile from "./Profile";
 
 const PoppingArtists = ({ mobile }) => {
   const [profileData, setProfileData] = useState({
@@ -34,20 +36,26 @@ const PoppingArtists = ({ mobile }) => {
     <Container
       className={`${styles.Content} ${mobile && "d-lg-none text-center mb-3"}`}
     >
-      <p>Most Poppin' Artists</p>
-      {mobile ? (
-        <div className="d-flex justify-content-around">
-          {poppingProfiles.results
-            .filter((profile) => profile.is_artist === true)
-            .slice(0, 4)
-            .map((profile) => (
-              <p key={profile.id}>{profile.owner}</p>
-            ))}
-        </div>
+      {poppingProfiles.results.length ? (
+        <>
+          <p>Our Most Popular Artists</p>
+          {mobile ? (
+            <div className="d-flex justify-content-around">
+              {poppingProfiles.results
+                .filter((profile) => profile.is_artist === true)
+                .slice(0, 4)
+                .map((profile) => (
+                  <Profile key={profile.id} profile={profile} mobile />
+                ))}
+            </div>
+          ) : (
+            poppingProfiles.results
+              .filter((profile) => profile.is_artist === true)
+              .map((profile) => <Profile key={profile.id} profile={profile} />)
+          )}
+        </>
       ) : (
-        poppingProfiles.results
-          .filter((profile) => profile.is_artist === true)
-          .map((profile) => <p key={profile.id}>{profile.owner}</p>)
+        <Asset spinner />
       )}
     </Container>
   );
