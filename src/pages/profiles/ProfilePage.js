ef02417f19468ec626d64af5a83cff25d9f4acd1
Ryan O'Neill <ryanoneill416@gmail.com>
2023-02-25 18:16:53 +0000

    Implement fetching and display of an artist's posts

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -19,6 +19,10 @@ import {
   useSetProfileData,
 } from "../../contexts/ProfileDataContext";
 import { Image } from "react-bootstrap";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Post from "../posts/Post";
+import { fetchMoreData } from "../../utils/utils";
+import noResults from "../../assets/noresults.png";
 
 function ProfilePage() {
   const [hasLoaded, setHasLoaded] = useState(false);
@@ -28,17 +32,20 @@ function ProfilePage() {
   const { pageProfile } = useProfileData();
   const [profile] = pageProfile.results;
   const is_owner = currentUser?.username === profile?.owner;
+  const [profilePosts, setProfilePosts] = useState({results: []})
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const [{ data: pageProfile }] = await Promise.all([
+        const [{ data: pageProfile }, {data: profilePosts}] = await Promise.all([
           axiosReq.get(`/profiles/${id}/`),
+          axiosReq.get(`/posts/?owner__profile=${id}`)
         ]);
         setProfileData((prevState) => ({
           ...prevState,
           pageProfile: { results: [pageProfile] },
         }));
+        setProfilePosts(profilePosts)
         setHasLoaded(true);
       } catch (err) {
         console.log(err);
@@ -98,7 +105,21 @@ function ProfilePage() {
     </>
   );
 
-  const profilePosts = <></>;
+  const profilePostDisplay = <>
+    {profilePosts.results.length ? (
+        <InfiniteScroll
+            children={profilePosts.results.map((post) => (
+                <Post key={post.id} {...post} setPosts={setProfilePosts} />
+            ))}
+            dataLength={profilePosts.results.length}
+            loader={<Asset spinner />}
+            hasMore={!!profilePosts.next}
+            next={() => fetchMoreData(profilePosts, setProfilePosts)}
+         />
+    ) : (
+        <Asset src={noResults} message={`No results found, ${profile?.owner} hasn't posted yet :)`}/>
+    )}
+  </>;
 
   return (
     <Row>
@@ -108,7 +129,7 @@ function ProfilePage() {
           {hasLoaded ? (
             <>
               {profileHeader}
-              {profilePosts}
+              {profilePostDisplay}
             </>
           ) : (
             <Asset spinner />
